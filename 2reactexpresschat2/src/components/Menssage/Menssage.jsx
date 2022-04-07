6c5244da97fe2d78d22f6d6ab27df674c459e417
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-07 13:38:30 +0200

    Menssage

---
+++ 2reactexpresschat2/src/components/Menssage/Menssage.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+
+
+function Menssage({id, password}) {
+    
+const [menssage, setMenssage] = useState ("");
+const token = authToken (id, password);
+const host = "https://web-develop-react-express-chat.herokuapp.com";
+
+
+function authToken(id, password) {
+
+    // En autenticación Basic, usuario y contraseña se separan con ':'
+    const authToken = `${id}:${password}`;
+    // Y se codifican en Base64
+    const base64token = btoa(authToken);
+    return `Basic ${base64token}`;
+}
+
+async function authGet(url, token) {
+    const response = await fetch(
+        url,
+        {
+            headers: {
+                Authorization: token
+            }
+        }
+    );
+    const data = await response.json();
+    return data;
+}
+
+function getMessage() {
+    authGet(host+"/messages/", token);
+}
+
+return (
+    <>
+        <h2> Contenido del Mensaje: </h2>
+        <button onClick={getMessage}> Recibir Mensajes</button>
+    </>
+)
+  
+}
+export default Menssage
